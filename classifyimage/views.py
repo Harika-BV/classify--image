20548a89f2e7f19cb58e69b4e3b3032ea12f7e96
Harika-BV <harikabhaskaram31@gmail.com>
2021-08-15 17:21:43 +0530

    Minor changes

--- classifyimage/views.py
+++ classifyimage/views.py
@@ -21,9 +21,10 @@ def getClassOfImage(image):
 
     classes = ('apple', 'aquarium_fish','baby','bear','beaver','bed','bee','beetle','bicycle','bottle','bowl','boy','bridge','bus','butterfly','camel','can','castle','caterpillar','cattle','chair','chimpanzee','clock','cloud','cockroach','couch','crab','crocodile','cup','dinosaur','dolphin','elephant','flatfish','forest','fox','girl','hamster','house','kangaroo','keyboard','lamp','lawn_mower','leopard','lion','lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom','oak_tree', 'orange','orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose','sea','seal','shark','shrew','skunk','skyscraper','snail','snake','spider','squirrel','streetcar','sunflower','sweet_pepper','table','tank','telephone','television','tiger','tractor','train','trout','tulip','turtle','wardrobe','whale','willow_tree','wolf', 'woman','worm')
 
-    PATH="classifyimage/models/ckpt_final.pth"
-
-    net.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
+    PATH="classifyimage/models/ckpt.pth"
+    checkpoint = torch.load(PATH, map_location=torch.device('cpu'))
+    net.load_state_dict(checkpoint['net'])
+    
     net.eval()
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
